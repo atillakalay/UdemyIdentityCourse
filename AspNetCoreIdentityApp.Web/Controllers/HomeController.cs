879f1e9d99d4eaeb2d40219cd16087543cbd94f0
atillakalay <atilla.kalayy@gmail.com>
2022-12-16 23:16:06 +0300

    The SignUp Action Method has been coded.

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +9,12 @@ namespace AspNetCoreIdentityApp.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -27,6 +31,30 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+
+            var identityResult = await _userManager.CreateAsync(
+                   new() { UserName = request.UserName, Email = request.Email, PhoneNumber = request.Phone },
+                   password: request.PasswordConfirm);
+
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarıyla gerçekleşmiştir.";
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            foreach (IdentityError item in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, item.Description);
+            }
+
+            return View();
+        }
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
