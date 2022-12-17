fbf26d4a87e742b9bbb99994f4aa502df14a8bfc
atillakalay <atilla.kalayy@gmail.com>
2022-12-17 12:40:33 +0300

    Password Validation Options Operations Are Done.

---
+++ AspNetCoreIdentityApp.Web/Extensions/StartupExtensions.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+
+namespace AspNetCoreIdentityApp.Web.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddIdentityWithExt(this IServiceCollection services)
+        {
+
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyz123456789_";
+
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+            }).AddEntityFrameworkStores<AppDbContext>();
+
+        }
+    }
+}
