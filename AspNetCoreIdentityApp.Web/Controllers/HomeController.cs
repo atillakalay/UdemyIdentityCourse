a252d92c91717e563a50cf612a1275b3838ee147
atillakalay <atilla.kalayy@gmail.com>
2022-12-18 15:43:08 +0300

    SignIn page was designed.

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace AspNetCoreIdentityApp.Web.Controllers
             return View();
         }
 
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
         public IActionResult SignUp()
         {
             return View();
--- AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -9,6 +9,7 @@ namespace AspNetCoreIdentityApp.Web.ViewModels
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Email alanı boş bırakılamaz.")]
+        [EmailAddress(ErrorMessage = "Email formatı yanlıştır.")]
         [Display(Name = "Email: ")]
         public string Email { get; set; }
 
