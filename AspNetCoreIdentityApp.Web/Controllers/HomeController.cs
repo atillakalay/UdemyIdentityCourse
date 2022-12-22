e490074161e90702bc86cc2ffc6c143b82ea1503
atillakalay <atilla.kalayy@gmail.com>
2022-12-22 22:12:51 +0300

    Bug fixed.

--- AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -134,22 +134,22 @@ namespace AspNetCoreIdentityApp.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> ResetPassword(ResetPasswordViewModel request)
         {
-            var userId = TempData["userId"]!.ToString();
-            var token = TempData["token"]!.ToString();
+            var userId = TempData["userId"];
+            var token = TempData["token"];
 
             if (userId == null || token == null)
             {
                 throw new Exception("Bir hata meydana geldi!");
             }
 
-            var hasUser = await _userManager.FindByIdAsync(userId);
+            var hasUser = await _userManager.FindByIdAsync(userId.ToString());
             if (hasUser == null)
             {
                 ModelState.AddModelError(String.Empty, "Kullanıcı bulunamamıştır.");
                 return View();
             }
 
-            var result = await _userManager.ResetPasswordAsync(hasUser, token, request.Password);
+            var result = await _userManager.ResetPasswordAsync(hasUser, token.ToString(), request.Password);
 
             if (result.Succeeded)
             {
