e9793a3348ce0f74dc4074da13746b6c2b1ac29f
unknown <atilla.kalayy@gmail.com>
2023-01-15 13:25:21 +0300

    Created PasswordChangeViewModel class.

---
+++ AspNetCoreIdentityApp.Web/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Eski şifre alanı boş bırakılamaz.")]
+        [Display(Name = "Eski Şifre: ")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karekter olabilir.")]
+        public string PasswordOld { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Yeni şifre alanı boş bırakılamaz.")]
+        [Display(Name = "Yeni Şifre: ")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karekter olabilir.")]
+        public string PasswordNew { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(PasswordNew), ErrorMessage = "Girmiş olduğunuz şifreler eşleşmemektedir.")]
+        [Required(ErrorMessage = "Şifre tekrar alanı boş bırakılamaz.")]
+        [Display(Name = "Şifre Tekrar: ")]
+        [MinLength(6, ErrorMessage = "Şifreniz en az 6 karekter olabilir.")]
+        public string PasswordConfirm { get; set; }
+    }
+}
