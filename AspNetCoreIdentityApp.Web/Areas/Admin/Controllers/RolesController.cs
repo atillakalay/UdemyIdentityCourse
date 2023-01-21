86f86a942a0b06dd90ac288e3d5215f1eaf20f9b
unknown <atilla.kalayy@gmail.com>
2023-01-21 13:36:39 +0300

    Authorization / Role Creation-1 Course Completed.

---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RolesController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
