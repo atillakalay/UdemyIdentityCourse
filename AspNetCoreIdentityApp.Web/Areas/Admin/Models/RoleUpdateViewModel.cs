76a03afb24d035fdf74e18dbe80965275865512f
unknown <atilla.kalayy@gmail.com>
2023-01-22 12:57:15 +0300

    Role Update lesson completed.

---
+++ AspNetCoreIdentityApp.Web/Areas/Admin/Models/RoleUpdateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.Areas.Admin.Models
+{
+    public class RoleUpdateViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Role isim alanı bpş bırakılamaz")]
+        [Display(Name = "Rol ismi: ")]
+        public string Name { get; set; }
+    }
+}
